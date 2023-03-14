98e38c91aa471246c084dd22e95089f35ab6e061
Saeed Forouzandeh <SaeedForouzandeh1@gmail.com>
2023-03-14 11:55:10 -0700

    first push

---
+++ script/script.js
@@ -0,0 +1,23 @@
+// const
+const wrapper = document.querySelector(".wrapper");
+const loginLink = document.querySelector(".loginLink");
+const registerLink = document.querySelector(".registerLink");
+const btnlogin = document.querySelector(".btnLogin-popup");
+const iconClose = document.querySelector(".iconClose");
+// register & Login
+registerLink.addEventListener("click", () => {
+  wrapper.classList.add("active");
+});
+
+loginLink.addEventListener("click", () => {
+  wrapper.classList.remove("active");
+});
+
+// Login btn
+btnlogin.addEventListener("click", () => {
+  wrapper.classList.add("active-popup");
+});
+
+iconClose.addEventListener("click", () => {
+  wrapper.classList.remove("active-popup");
+});
