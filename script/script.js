1243c93052e8aabe3ab6354d1f8d6d2a6b6697e6
Saeed Forouzandeh <SaeedForouzandeh1@gmail.com>
2023-03-17 06:27:50 -0700

    Update script.js

--- script/script.js
+++ script/script.js
@@ -1,3 +1,11 @@
+// function anime() {
+//   fetch("")
+//     .then((res) => res.json())
+//     .than((res) => {
+//       call(res);
+//     });
+// }
+
 // const
 const wrapper = document.querySelector(".wrapper");
 const loginLink = document.querySelector(".loginLink");
@@ -16,8 +24,22 @@ loginLink.addEventListener("click", () => {
 // Login btn
 btnlogin.addEventListener("click", () => {
   wrapper.classList.add("active-popup");
+  if (wrapper.classList.contains("active")) {
+    wrapper.classList.remove("active");
+  }
 });
 
 iconClose.addEventListener("click", () => {
   wrapper.classList.remove("active-popup");
 });
+
+// function episodes() {
+//   fetch("https://api.tvmaze.com/shows/82/episodes")
+//     .then((res) => res.json())
+//     .then((res) => {
+//       call(res);
+
+//       console.log(res);
+//     })
+//     .catch((err) => console.log(err));
+// }
